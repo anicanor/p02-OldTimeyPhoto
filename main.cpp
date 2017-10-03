4bc6d18eb193f69e0961b7902eb3b6da98d40e28
anicanor <anicanor@mail.csuchico.edu>
2017-10-02 17:52:40 -0700

    Wrote out instructions

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//Author: Aaron Nicanor
+#include <string>
+#include <vector>
+#include <bitmap.h>
+
+using namespace std;
+
+int main(){
+ /*
+ -Ask user for name of the file
+  -If the name of the file isn't right (not in the correct format or file doesn't exist),
+  ask again
+ -Store the users input if it's correct
+ -Create a matrix that is the same as the selected image
+ -Create a loop to go through the rows and a loop inside to go through the columns of
+ the selected image
+  -Inside the two loops change the RGB values so that it makes the image into greyscale and
+  save it into the clones matrix
+ -create a file and save the new image created in the clone matrix as oldtimey.bmp
+ */
+ return 0;
+}
