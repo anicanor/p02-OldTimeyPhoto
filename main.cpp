15c088aa678fba4ce982d6a8793c744bea3f275a
anicanor <anicanor@mail.csuchico.edu>
2017-10-16 20:00:25 -0700

    converts image to grayscale

--- main.cpp
+++ main.cpp
@@ -11,12 +11,10 @@ int main(){
   -If the name of the file isn't right (not in the correct format or file doesn't exist),
   ask again
  -Store the users input if it's correct
- -Create a matrix that is the same as the selected image
- -Create a loop to go through the rows and a loop inside to go through the columns of
- the selected image
-  -Inside the two loops change the RGB values so that it makes the image into greyscale and
-  save it into the clones matrix
- -create a file and save the new image created in the clone matrix as oldtimey.bmp
+ -Convert image into a pixel matrix
+ -Create a loop that'll look through each individual element of the pixel matrix
+  -Inside the two loops change the RGB values so that it makes the image into greyscale
+ -Save the pixel matrix as a new image called "oldtimey.bmp"
  */
  std::string name;
  Bitmap image;
@@ -27,16 +25,21 @@ int main(){
  double green;
  double blue;
  double gray;
-
+ 
+ //Obtains the user's image
  do{
-    cout<<"Enter name of image file (must be a bpm)"<<endl;
+
+    cout<<"Enter name of image file (remember to include .bmp at the end!)"<<endl;
     cin>>name;
     image.open(name);
+
  }while (image.isImage() == false);
 
+ //Tells the user the image's size
  bmp = image.toPixelMatrix();
  cout<<name<<" has been loaded. It is "<<bmp[0].size()<<" pixels wide and "<<bmp.size()<<" pixels high."<<endl;
 
+ //Converts image to grayscale
  for (int i = 0; i < bmp.size(); i++){
     for (int o = 0; o < bmp[0].size(); o++){
         rgb = bmp[i][o];
@@ -51,6 +54,7 @@ int main(){
     }
  }
  
+ //Saves image as "oldtimey.bmp"
  image.fromPixelMatrix(bmp);
  image.save("oldtimey.bmp");
  cout<<"File successfuly converted into grayscale."<<endl<<" Saved file name is oldtimey."<<endl;
